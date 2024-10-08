f1f947b808248860f49fbad3fee865a7dbfdc63a
Diego Anderson <diegoandersoncarva@hotmail.com>
2024-10-08 11:00:16 -0300

    Adicionei o hook useInterval e declarei o state pro mainTime

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from 'react'
+import { PomodoroTimer } from './components/pomodoro-timer'
 
 function App() {
-  return (
-    <div className="App">
-      Olá Mundo
-    </div>
-  );
+   return (
+      <div className="App">
+         <PomodoroTimer defaultPomodoroTime={1500} />
+      </div>
+   )
 }
 
-export default App;
+export default App
