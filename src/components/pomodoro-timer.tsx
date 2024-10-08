00b6bfd051f1f3adff1ae1604bb49c6843aac97c
Diego Anderson <diegoandersoncarva@hotmail.com>
2024-10-08 14:44:52 -0300

    Botão Play e Pause adicionado e etc

--- src/components/pomodoro-timer.tsx
+++ src/components/pomodoro-timer.tsx
@@ -1,35 +1,51 @@
-import React from 'react';
-import { useInterval } from '../hooks/use-interval';
-import { Button } from './button';
-import { Timer } from './timer';
+import React, { useEffect } from 'react'
+import { useInterval } from '../hooks/use-interval'
+import { Button } from './button'
+import { Timer } from './timer'
 
 interface Props {
-  pomodoroTime: number,
-  shortRestTime: number,
-  longRestTime: number,
-  cycles: number
+   pomodoroTime: number
+   shortRestTime: number
+   longRestTime: number
+   cycles: number
 }
 
 export function PomodoroTimer(props: Props): JSX.Element {
-  const [mainTime, setMainTime] = React.useState(props.pomodoroTime);
-  useInterval(()=> {
-    setMainTime(mainTime - 1);
-  }, 1000);
-  return (
-    <div className="pomodoro">
-      <h2>You are: working</h2>
-      <Timer mainTime = {mainTime} />
-      <div className="controls">
-      <Button text="teste" onClick={() => console.log('clicou')}></Button>
-      <Button text="teste" onClick={() => console.log('clicou')}></Button>
-      <Button text="teste" onClick={() => console.log('clicou')}></Button>
+   const [mainTime, setMainTime] = React.useState(props.pomodoroTime)
+   const [timeCounting, setTimeCounting] = React.useState(false)
+   const [working, setWorking] = React.useState(false)
+
+   useEffect(() => {
+      if (working) document.body.classList.add('working');
+   }, [working])
+
+   useInterval(
+      () => {
+         setMainTime(mainTime - 1)
+      },
+      timeCounting ? 1000 : null
+   )
+
+   const configureWork = (): void => {
+      setWorking(true);
+      setTimeCounting(true);
+   }
+
+   return (
+      <div className="pomodoro">
+         <h2>You are: working</h2>
+         <Timer mainTime={mainTime} />
+         <div className="controls">
+            <Button text="Work" onClick={() => configureWork()}></Button>
+            <Button text="teste" onClick={() => console.log('clicou')}></Button>
+            <Button text={timeCounting ? 'Pause' : 'Play'} onClick={() => setTimeCounting(!timeCounting)}></Button>
+         </div>
+         <div className="details">
+            <p>Testando outras coisas hahaha</p>
+            <p>Testando outras coisas hahaha</p>
+            <p>Testando outras coisas hahaha</p>
+            <p>Testando outras coisas hahaha</p>
+         </div>
       </div>
-      <div className="details">
-        <p>Testando outras coisas hahaha</p>
-        <p>Testando outras coisas hahaha</p>
-        <p>Testando outras coisas hahaha</p>
-        <p>Testando outras coisas hahaha</p>
-      </div>
-    </div>
-  )
+   )
 }
