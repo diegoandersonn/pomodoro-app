1d353bb654c1c2ad59497c8dd5b4d07b2540a2f2
Diego Anderson <diegoandersoncarva@hotmail.com>
2024-10-16 12:04:07 -0300

    code

--- src/pages/pomodoro.tsx
+++ src/pages/pomodoro.tsx
@@ -2,20 +2,18 @@ import React from 'react'
 import { PomodoroTimer } from '../components/pomodoro-timer'
 import Header from '../components/header'
 
-function App() {
+export default function Pomodoro() {
    return (
       <>
          <Header />
          <div className="container">
             <PomodoroTimer
-               pomodoroTime={1500}
-               shortRestTime={300}
-               longRestTime={900}
-               cycles={4}
+               pomodoroTime={Number(localStorage.getItem('pomodoroTime')) || 1500}
+               shortRestTime={Number(localStorage.getItem('shortRestTime')) || 300}
+               longRestTime={Number(localStorage.getItem('longRestTime')) || 900}
+               cycles={Number(localStorage.getItem('cycles')) || 4}
             />
          </div>
       </>
    )
 }
-
-export default App
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import App from '../pages/pomodoro'
+import Pomodoro from '../pages/pomodoro'
 import ConfigPage from '../pages/configPage'
 
 export default function MyRoutes() {
    return (
       <BrowserRouter>
          <Routes>
-            <Route path="/" element={<App />} />
+            <Route path="/" element={<Pomodoro />} />
             <Route path="/Config" element={<ConfigPage />} />
          </Routes>
       </BrowserRouter>
